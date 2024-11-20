44b23cd9ae0fff06139ca7b9da90234018c7cbe1
joaobotoni <joaobotoni@hotmail.com>
2024-11-20 15:30:25 -0400

    aws api gateway

--- RedirectUrlShortener/src/main/java/org/example/redirectUrlShortener/Main.java
+++ RedirectUrlShortener/src/main/java/org/example/redirectUrlShortener/Main.java
@@ -1,17 +1,77 @@
 package org.example.redirectUrlShortener;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    Map<String, Object> response = new HashMap<>();
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+
+        String pathParameters = (String) input.get("rawPath"); // Obtém o valor do parâmetro de caminho "raw-path" da rota requisitada.
+
+        // Verifica se pathParameters existe e não é vazio
+        if (pathParameters == null || pathParameters.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'rawPath' is required.");
         }
+
+        String shortUrlCode = pathParameters.replace("/", ""); // Obtém o valor do UUID.
+
+        if (shortUrlCode == null || shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
+        }
+
+        // Prepara a solicitação para buscar o dado no S3
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket("url-shortener-storage-testbot")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        InputStream s3Stream;
+
+        // Tenta buscar o dado do S3 e tratar erros
+        try {
+            s3Stream = s3Client.getObject(request);
+        } catch (AwsServiceException e) {
+            throw new RuntimeException("Error fetching data from S3: " + e.getMessage(), e);
+        }
+
+        UrlData data;
+
+        // Deserializa o JSON para o objeto UrlData
+        try {
+            data = objectMapper.readValue(s3Stream, UrlData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error deserializing URL data: " + e.getMessage(), e);
+        }
+
+        // Verifica se o URL expirou
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+
+        // Caso o URL tenha expirado, retorna um código 410
+        if ( data.getExpirationTime() < currentTimeInSeconds ) {
+            response.put("statusCode", 410); // Código 410 (Gone) para URL expirado
+            response.put("body", "The short URL has expired.");
+            return response;
+
+        }
+            response.put("statusCode", 302); // Código de status HTTP para redirecionamento
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Location", data.getOriginalUrl()); // URL de redirecionamento
+            response.put("headers", headers);
+
+        return response; // Retorna resposta para URL expirado
     }
 }
